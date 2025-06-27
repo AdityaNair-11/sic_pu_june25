cfce8f19f80eaa0e57da9253fdae81136df83195
AdityaNair-11 <findaditya.nair11@gmail.com>
2025-06-27 12:03:07 +0530

    problem solving

--- week1pythonbasics/assignment_on_loops/hallow_square.py
+++ week1pythonbasics/assignment_on_loops/hallow_square.py
@@ -1,3 +1,11 @@
 input = int(input('Enter the number of lines'))
-for i in range(input+1):
+for i in range(1,input+1):
+    for j in range(1,input+1):
+        if i == 1 or i == input or j == 1 or j == input:
+            print('*', end = " ")
+        else:
+            print(" ",end = " ")
+        
+
+    print()
     
